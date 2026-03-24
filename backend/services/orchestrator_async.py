e8a95a8ead66850d7faec4e60654354a14fd7f58
AYS <amreeshag5091@gmail.com>
2026-03-24 19:28:58 +0530

    add async orchestrator advanced

---
+++ backend/services/orchestrator_async.py
@@ -0,0 +1,34 @@
+from core.analyzer import classify
+from modules.domain import get_dns, get_subdomains
+from modules.scan import scan_ports
+from modules.username import check_username
+from modules.crawler import crawl
+from modules.tech import detect_tech
+from modules.dir_bruteforce import brute_dirs
+import asyncio
+
+async def run(query):
+    qtype = classify(query)
+    result = {"type": qtype}
+
+    if qtype == "domain":
+        tasks = await asyncio.gather(
+            asyncio.to_thread(get_dns, query),
+            asyncio.to_thread(get_subdomains, query),
+            asyncio.to_thread(scan_ports, query),
+            asyncio.to_thread(crawl, f"http://{query}"),
+            asyncio.to_thread(detect_tech, f"http://{query}"),
+            asyncio.to_thread(brute_dirs, query)
+        )
+
+        result["dns"] = tasks[0]
+        result["subdomains"] = tasks[1]
+        result["ports"] = tasks[2]
+        result["links"] = tasks[3]
+        result["tech"] = tasks[4]
+        result["directories"] = tasks[5]
+
+    elif qtype == "username":
+        result["profiles"] = check_username(query)
+
+    return result
